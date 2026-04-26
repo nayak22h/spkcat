7955e202458e95d1d1734b1064ae28c906642a57
nayak22h@yahoo.com <nayak22h@yahoo.com>
2026-04-26 23:17:36 +0400

    feat: implement dynamic product page loading with image carousel and PDF viewer support

--- Products/product.js
+++ Products/product.js
@@ -50,7 +50,7 @@ function loadProduct(productId) {
 
         // Render Breadcrumbs
         if (breadcrumbs) {
-            let breadcrumbHtml = `<a href="../index.html">Home</a>`;
+            let breadcrumbHtml = `<a href="../index.html"><svg class="home-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>Home</a>`;
             if (product.category) {
                 breadcrumbHtml += ` <span class="separator">/</span> <span class="category-crumb">${product.category}</span>`;
             }
